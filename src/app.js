8ff87a4c2afa52cb68b52fa559ce75eb94e01db0
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-07 22:01:30 +0530

    Added Health Check endpoint

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 import express, { urlencoded } from "express"
 import urlRouter from "./routes/url.route.js";
+import mongoose from "mongoose";
+import logger from "./utils/winstonLogger.js";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api/v1/url', urlRouter);
+
+app.get('/health', (_, res) => {
+    const dbStatus = mongoose.connection.readyState === 1 ? 'Connected' : 'Disconnected'; 
+
+    if(dbStatus === 'Disconnected'){
+        return res.status(503).json({
+            status: 'error',
+            database: 'disconnected'
+        })
+    }
+
+    res.status(200).json({
+        status: 'ok',
+        timestamp: new Date(),
+        uptime: process.uptime(),
+        database: dbStatus
+    });
+    
+})
 
 export default app;
