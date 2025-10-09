852e2255bd661e2d5d8eba4a822dcf21e30719ec
Varun0856 <Varun0856@users.noreply.github.com>
2025-10-09 23:05:08 +0530

    update rateLimit

--- src/middleware/rateLimiter.middleware.js
+++ src/middleware/rateLimiter.middleware.js
@@ -1,9 +1,9 @@
 import rateLimit from "express-rate-limit";
-import { RATE_LIMIT_MAX, RATE_LIMIT_WINDOW } from "../config/env";
+import { RATE_LIMIT_MAX, RATE_LIMIT_WINDOW } from "../config/env.js";
 
 const limiter = rateLimit({
-    windowMs: RATE_LIMIT_WINDOW,
-    limit: RATE_LIMIT_MAX,
+    windowMs: parseInt(RATE_LIMIT_WINDOW) || 60 * 60 * 1000,
+    limit: parseInt(RATE_LIMIT_MAX) || 100,
     message: "Too many requests, please try again later",
     standardHeaders: true,
     legacyHeaders: false
