2c1660d0e7fcf9595ad68cbe28ec7ffafd1824e8
Varun0856 <Varun0856@users.noreply.github.com>
2025-11-01 14:27:54 +0530

    update: include the global error handler middleware in the app.js

--- src/app.js
+++ src/app.js
@@ -1,42 +1,44 @@
-import express, { urlencoded } from "express"
+import express from "express"
 import urlRouter from "./routes/url.route.js";
 import mongoose from "mongoose";
+import errorMiddleware from "./middleware/error.middleware.js";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(errorMiddleware());
 
 app.use('/api/url', urlRouter);
 
 app.get('/health', (_, res) => {
-    const dbStatus = mongoose.connection.readyState === 1 ? 'Connected' : 'Disconnected'; 
+  const dbStatus = mongoose.connection.readyState === 1 ? 'Connected' : 'Disconnected';
 
-    if(dbStatus === 'Disconnected'){
-        return res.status(503).json({
-            status: 'error',
-            database: 'disconnected'
-        })
-    }
+  if (dbStatus === 'Disconnected') {
+    return res.status(503).json({
+      status: 'error',
+      database: 'disconnected'
+    })
+  }
 
-    res.status(200).json({
-        status: 'ok',
-        timestamp: new Date(),
-        uptime: process.uptime(),
-        database: dbStatus
-    });
-    
+  res.status(200).json({
+    status: 'ok',
+    timestamp: new Date(),
+    uptime: process.uptime(),
+    database: dbStatus
+  });
+
 })
 
 app.get('/', (_, res) => {
-    res.status(200).json({
-        name: 'URL_Shortener',
-        baseURL: '/api',
-        endpoints: {
-            shortenURl: 'POST /api/url/shorten',
-            redirectToOriginalUrl: 'GET /api/url/:shortId',
-            getAnalytics: 'GET /api/url/analytics/:shortId'
-        }
-    })
+  res.status(200).json({
+    name: 'URL_Shortener',
+    baseURL: '/api',
+    endpoints: {
+      shortenURl: 'POST /api/url/shorten',
+      redirectToOriginalUrl: 'GET /api/url/:shortId',
+      getAnalytics: 'GET /api/url/analytics/:shortId'
+    }
+  })
 })
 
 export default app;
